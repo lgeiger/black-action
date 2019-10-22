ba803f72fc8ad2398ea0a1c79a17c23d25600583
Alex <alex@vitalityvfx.com>
2019-10-22 11:15:20 -0700

    Switch Branch to HEAD_REF
    Add remote push url

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,15 +11,18 @@ then
   git init
 fi
 
+REMOTE_TOKEN_URL="https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
 if ! git remote | grep "origin" > /dev/null 2>&1
 then 
   echo "### Adding git remote..."
-  git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
+  git remote add origin $REMOTE_TOKEN_URL
   echo "### git fetch..."
   git fetch
 fi
 
-BRANCH="$GITHUB_REF"
+git remote set-url --push origin $REMOTE_TOKEN_URL
+
+BRANCH="$GITHUB_HEAD_REF"
 echo "### Branch: $BRANCH"
 git checkout $BRANCH
 
