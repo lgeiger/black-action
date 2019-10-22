42211ffdf08ad2833c62999334e497cb1db1cf65
Alex <alex@vitalityvfx.com>
2019-10-22 10:40:44 -0700

    Add git checks before running commands
    These git commands may error

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,21 @@ set -e
 # REF=$(jq -r ".ref" "$GITHUB_EVENT_PATH")
 # echo "Ref from JSON: $REF"
 
-echo "## Initializing git repo..."
-git init
-echo "### Adding git remote..."
-git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
-echo "### git fetch..."
-git fetch
-BRANCH=${GITHUB_REF#*refs/heads/}
+if ! git status > /dev/null 2>&1
+then
+  echo "## Initializing git repo..."
+  git init
+fi
+
+if ! git remote | grep "origin" > /dev/null 2>&1
+then 
+  echo "### Adding git remote..."
+  git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
+  echo "### git fetch..."
+  git fetch
+fi
+
+BRANCH="$GITHUB_REF"
 echo "### Branch: $BRANCH"
 git checkout $BRANCH
 
@@ -25,6 +33,6 @@ black $BLACK_ARGS
 echo "## Staging changes..."
 git add .
 echo "## Commiting files..."
-git commit -m "Formatted code" || true
+git commit -m "Black Automatically Formatted Code" || true
 echo "## Pushing to $BRANCH"
 git push -u origin $BRANCH
